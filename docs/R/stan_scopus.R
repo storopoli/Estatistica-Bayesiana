ecec0003c41f5c354effb67f24438617feebd6ed
Jose Eduardo Storopoli <thestoropoli@gmail.com>
2021-05-25 18:27:09 -0300

    Draft Slides and Typo Fix in probability definition

--- docs/R/stan_scopus.R
+++ docs/R/stan_scopus.R
@@ -1,8 +1,10 @@
 library(readr)
 library(dplyr)
 library(httr)
+library(purrr)
+library(rvest)
 library(stringr)
-library(jsonlite)
+library(tidyr)
 
 # Scopus query: ALL ( ( brms  AND  burkner )  OR  ( gelman  AND  hoffman  AND  stan )  OR
 # mc-stan.org  OR  rstanarm  OR  pystan  OR  ( rstan  AND NOT  mit ) )
@@ -21,6 +23,19 @@ df %>%
 df %>%
     summarise(citations = sum(cited_by, na.rm = TRUE))
 
+url <- read_html("https://breckbaldwin.github.io/ScientificSoftwareImpactMetrics/DeepLearningAndBayesianSoftware.html")
+
+url %>%
+    html_table() %>%
+    pluck(1) %>%
+    janitor::clean_names() %>%
+    mutate(across(stan:tensor_flow, ~as.numeric(str_extract(., "^(\\d*)")))) %>%
+    summarise(
+        stan = sum(stan, r_stan, r_stan_arm, py_stan, brms) / sum(detail_totals),
+        pymc = sum(py_mc) / sum(detail_totals),
+        tensorflow = sum(tensor_flow, keras) / sum(detail_totals),
+        pytorch = sum(py_torch) / sum(detail_totals)
+    )
 
 # Based on: https://breckbaldwin.github.io/CZI_NumFOCUS_materials/
 # base_url <- "https://api.elsevier.com/content/search/scopus"
